8b82c06d2df41be6f9e48fd13089a6e2a576f24c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-06 15:02:25 -0500

    If the string is a palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    // ***** My solution
+    // return str === (str.split('').reduce((rev,char) => char + rev, ''));
+    
+    // ***** Solution 1
+    // const reversed = str.split('').reverse().join('');
+    // return str === reversed;
+
+    // **** Solution 2
+    return str.split('').every((char, i) => {
+        return char == str[str.length -i - 1];
+    });
+    
+}
 
 module.exports = palindrome;
