31a5623a8290501f97b0aa09092797aa55b26b91
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-08 11:46:04 -0500

    How to reverse an int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,33 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    // ***** Solution 1
+    // let string = '';
+    // if(Math.sign(n) >= 0) {
+    //     string = n.toString().split('').reverse().join('');
+    //     debugger;
+    // }
+    // else {
+    //     const string2 = n.toString().split('-');
+    //     string = string2[1].split('').reverse().join('');
+    //     debugger;
+    //     return -Number(string);
+    // }
+    // return Number(string);
+
+    // ***** Solution 2
+    // const reversed = n.toString().split('').reverse().join('');
+    // if (n < 0) {
+    //     return parseInt(reversed) * -1;
+    // }
+    // return Number(reversed);
+
+    //***** Solution 3
+    const reversed = n.toString().split('').reverse().join('');
+    return Number(reversed) * Math.sign(n);
+
+}
+// reverseInt(500);  // for debugger statments
 
 module.exports = reverseInt;
