af7aa080820f55be719362f8fc0b283f47810bb1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-18 17:09:06 -0400

    Sentence Captilization - 1st Letter of every word

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,40 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // * My Solution
+  // const strArray =str.split('');
+  // let i=0;
+  // strArray[0]= strArray[0].toUpperCase();
+  // for (letter of strArray) {
+  //   if (letter === " "){
+  //     strArray[i+1] = strArray[i+1].toUpperCase();
+  //   };
+  //   i++;
+  // }
+  // return strArray.join('');
+
+  // * Solution 2
+  const words = [];
+  for ( let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(' ');
+
+  // let result = str[0].toUpperCase();
+
+  // for (let i =1; i < str.length; i++) {
+  //   if (str[i-1] === ' ') {
+  //     result += str[i].toUpperCase();
+  //   }
+  //   else {
+  //     result += str[i];
+  //   }
+  // }
+  // return result;
+
+
+}
 
 module.exports = capitalize;
