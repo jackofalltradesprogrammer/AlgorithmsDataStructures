93a1820e211d3d986109351f1951a295b4a53608
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-19 13:49:16 -0400

    Pyramid Steps

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  // Solution 1
+  // const center = Math.floor((2 * n - 1) / 2);
+  // for (let row = 0; row < n; row++) {
+  //   let stair = '';
+  //   for (let column = 0; column < 2 * n - 1; column++) {
+  //     if (center - row <= column && center + row >= column) {
+  //       stair += '#';
+  //     } else {
+  //       stair += ' ';
+  //     }
+  //   }
+  //   console.log(stair);
+  // }
+
+  // Solution 2
+  if (n === row) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const center = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (center - row <= level.length && center + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
+
+// pyramid(4);
 
 module.exports = pyramid;
