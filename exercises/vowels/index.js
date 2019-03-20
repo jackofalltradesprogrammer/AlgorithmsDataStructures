135a68187e5d201336282515b7eb71543ecf2d28
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-20 15:23:21 -0400

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // Solution 1
+  // let count = 0;
+  // const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  // for (let char of str.toLowerCase()) {
+  //   if (checker.includes(char)) {
+  //     count++;
+  //   }
+  // }
+
+  // return count;
+// match() returns the array of elements that matches with the given regex
+  const matches = str.match(/[aeiou]/gi); // g tells to keep going and not stop at the first match, i is for case-insensitive
+
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
