09d10b62d85be78b06bae1c1fbbeabd3497b97f0
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-24 13:56:56 -0400

    insertFirst, getF, size, clear, removeF, getLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -11,7 +11,48 @@ class Node {
 
 class LinkedList {
   constructor() {
+    // head holds a reference only and so it is similar to next in node
     this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let node = this.head;
+    if (!node) {
+      return null;
+    }
+    while (node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (this.head) {
+      this.head = this.head.next;
+    }
   }
 }
 
