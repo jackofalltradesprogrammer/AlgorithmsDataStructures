de8dbde337ebe866d8e1e0491365a297b7dd8e09
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-31 19:21:05 -0400

    Solving for Breadth-First Traversal

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -21,10 +21,23 @@ class Node {
   }
 
   remove(data) {
-    this.children = this.children.filter((node) => node.data !== data);
-  };
+    this.children = this.children.filter(node => node.data !== data);
+  }
 }
 
-class Tree {}
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
