10a46ee0682d22e2af3a09f3538854ff792a1361
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-04 11:28:04 -0400

    31. 32. BubbleSort, MergeSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,38 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[indexOfMin] > arr[j]) {
+        indexOfMin = j;
+        continue; // an optimization
+      }
+    }
+    if (indexOfMin !== i) {
+      const lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
-function mergeSort(arr) {
+function mergeSort(arr) {}
 
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
