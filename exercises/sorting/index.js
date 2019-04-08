29af4bee79711ed3758ef1c1bf5d8002189470a5
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-07 20:13:53 -0400

    MergeSort (Divide and conquer)

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -32,8 +32,27 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr, start=0, end=arr.length) {
+  if(arr.length === 1){
+    return arr;
+  }
+  const center = Math.floor(arr.length/2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center, arr.length);
+  return merge(mergeSort(left), mergeSort(right));
+}
 
-function merge(left, right) {}
+function merge(left, right) {
+  const results = [];
+  while(left.length && right.length){
+    if (left[0] < right[0]){
+      results.push(left.shift());
+    }
+    else {
+      results.push(right.shift());
+    }
+  }
+  return [...results, ...left,...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
